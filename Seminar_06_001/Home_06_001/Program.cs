5afaebaa30f6ec74681addeadf0aff88ca9eb9eb
Ekaterina <Kuteynikova.ekaterina@mail.ru>
2022-06-04 20:49:38 +0300

    Home_0002

---
+++ Seminar_06_001/Home_06_001/Program.cs
@@ -0,0 +1,38 @@
+﻿//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} "); 
+    }
+    Console.WriteLine();
+}
+
+int[] SelectionSort(int M, int from, int to)
+{
+    int[] arr = new int[M];
+    for (int i = 0; i < M; i++)
+    {
+        arr[i] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+void CountNum(int[] arr)
+{
+    int SumPlus = 0;
+    int size = arr.Length;
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > 0) SumPlus++;
+    }
+    Console.WriteLine(SumPlus);
+
+}
+
+
+int[] arr_1 = SelectionSort(10, -99, +100);
+Print(arr_1);
+CountNum(arr_1);
