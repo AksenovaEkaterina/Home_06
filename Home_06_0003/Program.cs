9f9687d4e1dc89199f95447810794f8b20534a55
Ekaterina <Kuteynikova.ekaterina@mail.ru>
2022-06-04 21:30:36 +0300

    Home_0003

---
+++ Home_06_0003/Program.cs
@@ -0,0 +1,8 @@
+﻿void IntersectionPoint (double k1, double b1, double k2, double b2)
+{
+
+    double x = (b2-b1)/(k1-k2);
+    double y = k1*x + b1;
+    Console.WriteLine ($" {x}; {y}");
+}
+IntersectionPoint (5, 2, 9, 4);
